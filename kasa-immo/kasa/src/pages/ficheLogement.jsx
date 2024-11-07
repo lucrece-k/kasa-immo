61052a877a502c6f42890b8613959dbb409067e6
kouame lucrèce <kouamelucrece@gmail.com>
2024-11-07 01:47:37 +0100

    ajout slide dans ficheLogement

--- kasa-immo/kasa/src/pages/ficheLogement.jsx
+++ kasa-immo/kasa/src/pages/ficheLogement.jsx
@@ -1,22 +1,59 @@
 import "../components/index.scss";
 import listeLogement from "../listeLogement.json";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import arrowLeft from "../images/arrowLeft.png";
+import arrowRight from "../images/arrowRight.png";
 
-function FicheLogement() {
+function FicheLogement(showArrow = true, showNumber = true) {
   const { id } = useParams();
   const logement = listeLogement.find((logement) => logement.id === id);
+  const isCoverArray = Array.isArray(logement.pictures);
+
+  const [actifPhoto, setactifPhoto] = useState(0);
+  const clickArrowRight = () => {
+    setactifPhoto(
+      (i) => (i + 1) % (isCoverArray ? logement.pictures.length : 1)
+    );
+  };
+
+  const clickArrowLeft = () => {
+    setactifPhoto(
+      (i) =>
+        (i - 1 + (isCoverArray ? logement.pictures.length : 1)) %
+        (isCoverArray ? logement.pictures.length : 1)
+    );
+  };
+
   return (
     <div className="ks-main">
-      <div className="div-arrow">
-        <img className="arrow" src="../assets/arrowLeft.png" alt="arrow left" />
-        <img
-          className="arrow"
-          src="../assets/arrowRight.png"
-          alt="arrow right"
-        />
-      </div>
       <div className="div-slide">
-        <img src={logement.cover} alt={logement.title} />
+        {showArrow && (
+          <img
+            className="arrow arrowLeft"
+            src={arrowLeft}
+            alt="arrow left"
+            onClick={clickArrowLeft}
+          />
+        )}
+        <img
+          className="photo"
+          src={isCoverArray ? logement.pictures[actifPhoto] : logement.pictures}
+          alt={logement.title}
+        />
+        {showArrow && (
+          <img
+            className="arrow arrowRight"
+            src={arrowRight}
+            alt="arrow right"
+            onClick={clickArrowRight}
+          />
+        )}
+        {showNumber && (
+          <span className="numeroPhoto">
+            {[actifPhoto] - logement.pictures.length}
+          </span>
+        )}
       </div>
     </div>
   );
